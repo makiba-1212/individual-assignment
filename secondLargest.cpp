91d177ed4c86377300ca215820160f22ccc9cb45
makiba-1212 <miserfirde@gmail.com>
2025-04-08 08:45:26 +0300

    Update secondLargest.cpp

--- secondLargest.cpp
+++ secondLargest.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 using namespace std;
-int counter = 0; // Global variable to count iterations
+int counter = 0; 
 // Define the function first
 int linearSearch(int arr[], int size, int searchItem) {
     for (int i = 0, j = size - 1; i <= j; i++, j--) { 
-        counter++; // Counting iterations
-        if (arr[i] == searchItem)  // Checking from left side
+        counter++; 
+        if (arr[i] == searchItem)  
             return i;  
 
-        if (arr[j] == searchItem)  // Checking from right side
+        if (arr[j] == searchItem) 
             return j;  
     }
-    return -1; // If not found
+    return -1; // 
 }
 int main() {
-    // Sample array
+
     int inputArr[] = {6, 2, 32, 11, 8, 22, 16, 20, 5, 25}; 
-    int size = sizeof(inputArr) / sizeof(inputArr[0]); // Calculate array size
-    int item = 0; // Searching for 20
-    // cin >> item; // Uncomment to take user input
-    int dataIndex = linearSearch(inputArr, size, item); // Call search function
-    // Display results
+    int size = sizeof(inputArr) / sizeof(inputArr[0]); 
+    int item = 0; 
+    
+    int dataIndex = linearSearch(inputArr, size, item); 
+
     if (dataIndex == -1) {
         cout << "Element not found!!!" << endl;
     } else {
         cout << "Element found at index: " << dataIndex << endl;
     }
-    cout << "Total number of iterations: " << counter << endl; // Display number of iterations
+    cout << "Total number of iterations: " << counter << endl; 
     return 0;
 }
